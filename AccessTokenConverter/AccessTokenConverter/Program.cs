4e7228e9919463855a8a014387d5c4a5273f94e2
ChalChalCh <k.calvin567@gmail.com>
2023-08-25 10:38:49 +0530

    Get Necessary Details from JWT Token

---
+++ AccessTokenConverter/AccessTokenConverter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // this is a sample access token geneareted from jwt.io , replace it with real access token generated after the OTP validation
+        string accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        string userEmail = GetEmailFromJwt(accessToken);
+
+        if (!string.IsNullOrEmpty(userEmail))
+        {
+            Debug.WriteLine($"User's email: {userEmail}");
+        }
+        else
+        {
+            Debug.WriteLine("Failed to retrieve user's email.");
+        }
+    }
+
+    public static string GetEmailFromJwt(string jwtToken)
+    {            
+        JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+        JwtSecurityToken jwt = tokenHandler.ReadJwtToken(jwtToken);
+
+        // Extract the required data from the JWT token
+        string userEmail = jwt.Payload["name"]?.ToString();
+        return userEmail;
+    }
+}
