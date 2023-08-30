ef6ba1a635bce6f249b3eaf6e55e7285652b33cd
ChalChalCh <k.calvin567@gmail.com>
2023-08-30 12:43:52 +0530

    Trying Dynamic

--- HTMLToWord/HTMLToWord/Program.cs
+++ HTMLToWord/HTMLToWord/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using Aspose.Words;
 using Aspose.Words.Saving;
+using DocumentFormat.OpenXml.Math;
+using DocumentFormat.OpenXml.Packaging;
 
 namespace HTMLToWordConversionWithAspose
 {
@@ -10,10 +13,23 @@ namespace HTMLToWordConversionWithAspose
         static void Main(string[] args)
         {
             string htmlFilePath = "D:\\github\\Converter\\HTMLToWord\\HTMLToWord\\HTMLDocumentWithStyleAndGraph.html";
-            string outputDirectory = "D:\\github\\Converter\\HTMLToWord\\HTMLToWord"; 
+            string dynamicHTMLFilePath = "D:\\github\\Converter\\HTMLToWord\\HTMLToWord\\Dynamic.html";
+            string outputDirectory = "D:\\github\\Converter\\HTMLToWord\\HTMLToWord";
+
+
+
+            //dynamic data conversion and data binding
+            //string dTitle = "Dynamic Title";
+            //string dHTMLFilePath = File.ReadAllText(dynamicHTMLFilePath);
+            //byte[] htmlBytes = Encoding.UTF8.GetBytes(dHTMLFilePath);
+            //string dContent = "This is dynamic content.";
+            //byte[] modifiedDocumentBytes = ReplacePlaceholders(htmlBytes, dTitle, dContent);
+            //File.WriteAllBytes(outputDirectory,modifiedDocumentBytes);
+
 
             // Read HTML content
             string htmlContent = File.ReadAllText(htmlFilePath);
+
 
             // File generated based on TimeStamp
             string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
@@ -33,6 +49,32 @@ namespace HTMLToWordConversionWithAspose
             
 
             doc.Save(outputFilePath, SaveFormat.Docx);
+        }
+
+        static byte[] ReplacePlaceholders(byte[] templateBytes, string title, string content)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                stream.Write(templateBytes, 0, templateBytes.Length);
+
+                using (WordprocessingDocument document = WordprocessingDocument.Open(stream, true))
+                {
+                    foreach (Text text in document.MainDocumentPart.Document.Body.Descendants<Text>())
+                    {
+                        if (text.Text.Contains("<<Title>>"))
+                        {
+                            text.Text = text.Text.Replace("<<Title>>", title);
+                        }
+
+                        if (text.Text.Contains("<<Content>>"))
+                        {
+                            text.Text = text.Text.Replace("<<Content>>", content);
+                        }
+                    }
+                }
+
+                return stream.ToArray();
+            }
         }
     }
 }
